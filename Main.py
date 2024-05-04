00251a562f6d08482a6ed052960ee6df3188eb59
Sxlly <shaesullivan.buisness1@gmail.com>
2024-05-04 16:23:48 +0800

    adding xml pretrained haar cascade datasets

--- Main.py
+++ Main.py
@@ -5,3 +5,7 @@ from random import *
 
 #*** import demo car image file to test detection
 initImg = "demoCarImg.jpg"
+#*** import pretrained xml to train processor how to detect cars accuratley {machine learning}
+pretrainedCars = "carsDataset.xml"
+#*** import pretrained xml to train processor how to detect cars accuratley {machine learning}
+pretrainedPedestrains = "haarcascadePedestrianDataSet.xml"
