31f80af40b7aa58ef34de044f0a8390f2ffbae70
Sxlly <shaesullivan.buisness1@gmail.com>
2024-05-04 16:52:10 +0800

    adding image with human and car in same frame (yes the guy has stopped to take a piss...)

--- Main.py
+++ Main.py
@@ -4,8 +4,15 @@ from random import *
 
 
 #*** import demo car image file to test detection
-initImg = "demoCarImg.jpg"
+initImg = "freewaypeebreak.jpeg"
 #*** import pretrained xml to train processor how to detect cars accuratley {machine learning}
 pretrainedCars = "carsDataset.xml"
 #*** import pretrained xml to train processor how to detect cars accuratley {machine learning}
 pretrainedPedestrains = "haarcascadePedestrianDataSet.xml"
+
+
+#*** create opencv image
+openImg = cv2.imread(initImg)
+
+cv2.imshow("Basic AutoPilot AI", openImg)
+cv2.waitKey()
